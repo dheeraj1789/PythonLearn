baeda8d3eff5c941813dcf848eabd4f5158969bd
Dheeraj Yadav <d2dimhere@gmail.com>
2026-07-13 14:09:07 +0530

    Added patterns

---
+++ Patterns.py
@@ -0,0 +1,43 @@
+#Solid rectangle
+rows = 7
+cols = 8
+for i in range(rows):
+    for j in range(cols):
+        print("*",end=" ")
+    print() 
+rows = 5
+#trangle
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print() 
+
+#hallow ractangle
+
+rows = 5
+cols = 6
+for i in range(rows):
+    for j in range(cols):
+        if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+#Revtangle with number
+rows = 4    
+cols = 5
+for i in range(1, rows + 1):
+    for j in range(1, cols + 1):
+        print(j, end=" ")
+    print()
+    
+#Hallow triangle
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        if j == 1 or j == i or i == rows:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
