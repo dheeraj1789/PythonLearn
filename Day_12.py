8e000f0e1f03383d9afc3bed42bbff4f5aeb3a9f
Dheeraj Yadav <d2dimhere@gmail.com>
2026-07-18 11:56:57 +0530

    File operation

---
+++ Day_12.py
@@ -0,0 +1,11 @@
+with open("Day12.txt","r+") as fw:
+    data = fw.read().splitlines()
+    print(data)
+
+#with open("Day12.txt","w") as fw:
+    fw.write("Hello, World!\n")
+   # fw.write("This is my first time writing to a file in Python.")
+    #print(data)
+   # print(data[0])
+   # print(data[1])
+   # print("this is my first time reading from a file in Python.")
